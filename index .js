232688baf748b19c90658f23e86261b0c80a0e62
Imran khan <adhurekhavab@gmail.com>
2023-01-13 00:54:35 +0530

    final commit

---
+++ index .js
@@ -0,0 +1,9 @@
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+const age = document.getElementById("age");
+const gender = document.getElementById("gender");
+const activity = document.getElementById("activity");
+const form = document.getElementById("mealForm");
+
+const submitbtn = document.getElementById("submitBtn");
+
